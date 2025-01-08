a26354d92a6bbe1517d11ac623ea41c9719f737d
chibuike <chrisroyalty001@gmail.com>
2025-01-08 18:36:20 +0100

    update on basic structure for home page

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/digital-aba/", // Replace 'repository-name' with your GitHub repo name
+  base: "/", // Replace 'repository-name' with your GitHub repo name
 });
