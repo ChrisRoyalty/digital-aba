ed3c20ce75ea4937cb63d8f79c30d45bb41bf702
chibuike <chrisroyalty001@gmail.com>
2025-01-16 13:33:37 +0100

    updated carousel for hero section of home page

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -65,12 +65,13 @@ const HeroSection = () => {
             <strong>Digital </strong>
             <span className="text-blue-300">{currentText}</span>
           </h1>
-          <p className="mt-4 text-[16px] lg:text-[20px] lg:w-[90%] text-gray-200 lg:text-[#C7C7CC] max-sm:bg-gradient-to-r from-black/75 via-black/30 p-2">
+          <p className="mt-4 text-[16px] lg:text-[20px] lg:w-[90%] text-gray-200 lg:text-[#C7C7CC] max-sm:p-4 max-sm:bg-gradient-to-b from-transparent via-black/85 to-transparent">
             Digital Aba is a one-stop digital support center for non-tech
             businesses in Aba. We help connect you with the right digital tools,
             training, and skilled workers to help your business grow and succeed
             in today’s digital world.
           </p>
+
           <div className="mt-6">
             <a
               href="https://wa.link/uq3khh"
